56c11892a5c5dbbd81d710825de63c08cea35460
quartneyquo <31452814+quartneyquo@users.noreply.github.com>
2019-02-21 23:15:14 -0800

    Update script.js

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -122,7 +122,7 @@ $("#navbarToggle").blur(function (event) {
           // Use the existing insertHtml function for that purpose. Look through this code for an example
           // of how to do that.
           // ....
-          insertHtml(selector, homeHtmlToInsertIntoMainPage);
+          insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
         },
         false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
       }
